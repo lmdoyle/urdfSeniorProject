c8d889a0dc440da5b1474c682db1e7162ad17204
Ezra Brooks <ezra@brooks.cx>
2024-02-19 13:17:52 -0700

    add first cut at floating joint support

--- javascript/src/URDFClasses.js
+++ javascript/src/URDFClasses.js
@@ -1,6 +1,7 @@
-import { Object3D, Vector3 } from 'three';
+import { Euler, Object3D, Vector3 } from 'three';
 
 const _tempAxis = new Vector3();
+const _tempEuler = new Euler();
 
 class URDFBase extends Object3D {
 
@@ -242,16 +243,55 @@ class URDFJoint extends URDFBase {
             }
 
             case 'floating':
-                // TODO: Support these joint types
-                console.warn(`'${ this.jointType }' joint not yet supported`);
-                break;
+                // no-op if all values are identical to existing value or are null
+                if (this.jointValue.every((value, index) => values[index] === value || values[index] === null)) return didUpdate;
+                // anonymous block scope to prevent variable name clobbering with other cases in the switch
+                {
+                    // Floating joints have six degrees of freedom: X, Y, Z, R, P, Y.
+                    const [posX, posY, posZ, roll, pitch, yaw] = values;
+                    this.position.copy(this.origPosition);
+                    // Respect origin RPY when setting position
+                    if (posX !== this.jointValue[0] && posX !== null) {
+                        _tempAxis.set(1, 0, 0).applyEuler(this.rotation);
+                        this.position.addScaledVector(_tempAxis, posX);
+                        didUpdate = true;
+                    }
+                    if (posY !== this.jointValue[1] && posY !== null) {
+                        _tempAxis.set(0, 1, 0).applyEuler(this.rotation);
+                        this.position.addScaledVector(_tempAxis, posY);
+                        didUpdate = true;
+                    }
+                    if (posZ !== this.jointValue[2] && posZ !== null) {
+                        _tempAxis.set(0, 0, 1).applyEuler(this.rotation);
+                        this.position.addScaledVector(_tempAxis, posZ);
+                        didUpdate = true;
+                    }
+
+                    // Doing the math to set each individual Euler value seemed more awkward than just handling all this in the same case using ternaries.
+                    if (roll !== this.jointValue[3] || pitch !== this.jointValue[4] || yaw !== this.jointValue[5]) {
+                        this.jointValue[3] = roll !== null ? roll : this.jointValue[3];
+                        this.jointValue[4] = pitch !== null ? pitch : this.jointValue[4];
+                        this.jointValue[5] = yaw !== null ? yaw : this.jointValue[5];
+                        this.quaternion.setFromEuler(
+                            _tempEuler.set(
+                                this.jointValue[3],
+                                this.jointValue[4],
+                                this.jointValue[5],
+                                'XYZ',
+                            )).premultiply(this.origQuaternion);
+                        didUpdate = true;
+                    }
+                }
+
+                this.matrixWorldNeedsUpdate = didUpdate;
+                return didUpdate;
+
             case 'planar':
                 // no-op if all values are identical to existing value or are null
                 if (this.jointValue.every((value, index) => values[index] === value || values[index] === null)) return didUpdate;
 
                 // Planar joints have two degrees of freedom: X distance, Y distance
-                const posX = values[0];
-                const posY = values[1];
+                const [posX, posY] = values;
 
                 // Respect existing RPY when modifying the position of the X,Y axes
                 this.position.copy(this.origPosition);
@@ -260,16 +300,15 @@ class URDFJoint extends URDFBase {
                     this.position.addScaledVector(_tempAxis, posX);
                     this.jointValue[0] = posX;
                     didUpdate = true;
-                    this.matrixWorldNeedsUpdate = true;
                 }
                 if (posY !== null) {
                     _tempAxis.set(1, 0, 0).applyEuler(this.rotation);
                     this.position.addScaledVector(_tempAxis, posY);
                     this.jointValue[1] = posY;
                     didUpdate = true;
-                    this.matrixWorldNeedsUpdate = true;
                 }
 
+                this.matrixWorldNeedsUpdate = didUpdate;
                 return didUpdate;
 
         }
